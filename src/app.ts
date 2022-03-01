8b3ba86be66cd7baa09705f19cb5da1e980c6646
kelvin <kelvininfortec@gmail.com>
2022-03-01 15:31:24 -0300

    install cors

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,19 @@ import { connectDatabase } from './database';
 import swaggerUiExpress from 'swagger-ui-express';
 import swaggerDocument from './docs/swagger.json';
 import "reflect-metadata";
+import cors from 'cors';
 
 connectDatabase();
 
 const app = express();
 
 app.use(express.json())
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,DELETE");
+    app.use(cors());
+    next();
+})
 app.use('/api-docs', swaggerUiExpress.serve, swaggerUiExpress.setup(swaggerDocument));
 
 InitializerRouter(app);
